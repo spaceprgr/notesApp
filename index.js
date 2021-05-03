79b54fdfc6919ed15a8c9020c08f24fdf829cdaa
reciprogms <rashad.jsp@gmail.com>
2021-05-02 22:40:03 -0700

    the final working version notesApp

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ function getNoteId() {
 	if(!noteObject){
 		return 1
 	}
-	const keysArray = Object.keys(noteObject)
-	const numberKeys = keysArray.map((key)=>Number(key))
-	cinsole.log(numberKeys)
-	return Math.max( ... numberKeys) + 1
+	
+	const keysArray = Object.keys(noteObject);
+	const numberKeys = keysArray.map((key) => Number(key))
+	console.log(numberKeys);
+	return Math.max( ... numberKeys) + 1;
 }
 
 function getExistingNotes() {
-	let notes = localStoraage.getItem("notes")
+	let notes = localStorage.getItem('notes')
 	if(!notes){
 		return null
 	}
-	return JSON.parse(notes)
+	return JSON.parse(notes);
 }
